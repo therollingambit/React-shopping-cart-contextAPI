ebbec7b4fb7fcc0eabb855a85284c6b04994f646
Ahmad Enayathullah <ahmad.enayathullah@fdmgroup.com>
2021-07-31 18:33:42 +0800

    first commit

---
+++ src/components/Filters.js
@@ -0,0 +1,90 @@
+import { Form, Button } from "react-bootstrap";
+import { CartState } from "../context/Context";
+import Rating from "./Rating";
+
+const Filters = () => {
+
+  const {productState: {
+    byStock,
+    byFastDelivery,
+    byRating,
+    sort,
+  }, productDispatch} = CartState()
+
+  return (
+    <div className="filters">
+      <span className="title">Filter Products</span>
+      <span>
+        <Form.Check
+          inline
+          label="Ascending"
+          name="group1"
+          type="radio"
+          id={`inline-1`}
+          onChange={() => productDispatch({type: 'SORT_BY_PRICE', payload: 'lowToHigh'})}
+          checked={sort === 'lowToHigh' ? true : false}
+        />
+      </span>
+      <span>
+        <Form.Check
+          inline
+          label="Descending"
+          name="group1"
+          type="radio"
+          id={`inline-2`}
+          onChange={() => productDispatch({type: 'SORT_BY_PRICE', payload: 'highToLow'})}
+          checked={sort === "highToLow" ? true : false}
+        />
+      </span>
+
+      <span>
+        <Form.Check
+          inline
+          label="Include out of stock"
+          name="group1"
+          type="checkbox"
+          id={`inline-3`}
+          onChange={() =>
+            productDispatch({
+              type: "FILTER_BY_STOCK",
+            })
+          }
+          checked={byStock}
+        />
+      </span>
+      <span>
+        <Form.Check
+          inline
+          label="Fast Delivery Only"
+          name="group1"
+          type="checkbox"
+          id={`inline-4`}
+          onChange={() =>
+            productDispatch({
+              type: "FILTER_BY_DELIVERY",
+            })
+          }
+          checked={byFastDelivery}
+        />
+      </span>
+      <span>
+        <label style={{ paddingRight: 10 }}>Rating: </label>
+        <Rating rating={byRating} style={{ cursor: 'pointer' }} onClick={(i) =>
+          productDispatch({
+            type: "FILTER_BY_RATING",
+            payload: i + 1,
+          })} />
+      </span>
+      <Button
+        variant='light'
+        onClick={() =>
+          productDispatch({
+            type: "CLEAR_FILTERS",
+          })
+        }
+      >Clear filters</Button>
+    </div>
+  );
+};
+
+export default Filters;
---
+++ src/components/Product.js
@@ -0,0 +1,44 @@
+import { Button, Card } from "react-bootstrap";
+import Rating from "./Rating";
+import { RiDeleteBin5Line } from "react-icons/ri";
+import { CartState } from "../context/Context";
+
+const Product = ({ product }) => {
+  const {
+    state: { cart },
+    dispatch,
+  } = CartState();
+
+  return (
+    <div className="products">
+      <Card>
+        <Card.Img variant="top" src={product.image} alt={product.name} />
+        <Card.Body>
+          <Card.Title>{product.name}</Card.Title>
+          <Card.Subtitle style={{ paddingBottom: 10 }}>
+            <span>$ {product.price.split(".")[0]}</span>
+            {product.fastDelivery ? (
+              <div>Fast delivery</div>
+            ) : (
+              <div>4 days delivery</div>
+            )}
+            <Rating rating={product.ratings} />
+          </Card.Subtitle>
+          {cart.some((p) => p.id === product.id) ? (
+            <Button variant="danger" onClick={() =>
+              dispatch({type: 'REMOVE_FROM_CART', payload: product})}>
+              <RiDeleteBin5Line />
+            </Button>
+          ) : (
+            <Button disabled={!product.inStock} onClick={() =>
+              dispatch({type: 'ADD_TO_CART', payload: product})}>
+              {!product.inStock ? "Out of stock" : "Add to cart"}
+            </Button>
+          )}
+        </Card.Body>
+      </Card>
+    </div>
+  );
+};
+
+export default Product;
